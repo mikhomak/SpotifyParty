f3b8879fb6a61dcfaa4b06fb585cedbae01510f9
misha <homakhaker@gmail.com>
2020-05-07 23:23:43 +0200

    Added volume controls
    A

--- front/src/components/footer/Footer.tsx
+++ front/src/components/footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import './Footer.css'
 import {PlayerControls} from "../playerControls/PlayerControls";
 import {NowPlaying} from "./nowPlaying/NowPlaying";
+import {VolumeControls} from "./volumeControls/VolumeControls";
 
 export const Footer = () => {
 
@@ -10,6 +11,7 @@ export const Footer = () => {
             <NowPlaying songName='The only exception' artistName='Paramore'
                         diskImageUrl='https://i.scdn.co/image/ab67616d00001e020b73f9c0b28314ae14ad9e20'/>
             <PlayerControls/>
+            <VolumeControls/>
         </div>
     );
 
---
+++ front/src/components/footer/volumeControls/VolumeControls.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './VolumeControls.css'
+
+export const VolumeControls = () => {
+
+    return (
+        <div className="VolumeControls">
+            <span>sad</span>
+        </div>
+    );
+};
