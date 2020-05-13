b0ea4ce20a261ef780f1ed60b4e57ef92520f391
Misha <homakhaker@gmail.com>
2020-05-13 22:59:52 +0200

    Created playlist pass

---
+++ front/src/models/playlistPass/PlaylistPass.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './PlaylistPass.css';
+
+interface PlaylistPassProps{
+
+};
+
+
+interface PlaylistPassState {
+
+};
+
+
+export class PlaylistPass extends React.Component<PlaylistPassProps, PlaylistPassState> {
+
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    };
+};
