b9c7f64b1e29b0f846eadd727ff06e75a472810d
Misha <homakhaker@gmail.com>
2020-05-19 10:34:54 +0200

    Added ol to the right bar

--- front/src/components/rightBar/topParty/TopParty.tsx
+++ front/src/components/rightBar/topParty/TopParty.tsx
@@ -8,13 +8,17 @@ interface TopPartyProps {
 }
 
 
-export const TopParty:React.FC<TopPartyProps> = (props) => {
+export const TopParty: React.FC<TopPartyProps> = (props) => {
 
     return (
         <div className='TopParty'>
-            <span>{props.views}</span> &nbsp;
-            <span>{props.name}</span> <br/>
-            <span>{props.currentSong}</span>
+            <div className='FirstRow'>
+                <span>{props.name}</span> <br />
+            </div>
+            <div className='SecondRow'>
+                <span>{props.views}</span> &nbsp;
+                <span>{props.currentSong}</span>
+            </div>
         </div>
     );
 };
