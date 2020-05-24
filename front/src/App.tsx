730aae360993c778b7dbd9fcf793dcf6e7e76fe3
Misha <homakhaker@gmail.com>
2020-05-24 15:41:03 +0200

    added spotify api

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import './App.css';
-import {Main} from "./models/main/Main";
-import {HashRouter as Router, Route, Switch} from 'react-router-dom';
-import {Navbar} from "./components/navbar/Navbar";
-import {Footer} from "./components/footer/Footer";
-import {About} from "./models/about/About";
-import {Party} from "./models/party/Party";
+import { Main } from "./models/main/Main";
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { Navbar } from "./components/navbar/Navbar";
+import { Footer } from "./components/footer/Footer";
+import { About } from "./models/about/About";
+import { Party } from "./models/party/Party";
 import { PlaylistPass } from './models/playlistPass/PlaylistPass';
 import { RightBar } from './components/rightBar/RightBar';
 
 function App() {
     return (
         <Router>
-            <Navbar/>
+            <Navbar />
             <div className="App">
                 <Switch>
-                    <Route exact path="/" component={Main}/>
-                    <Route exact path="/create" component={Main}/>
-                    <Route exact path="/join" component={Main}/>
-                    <Route exact path="/about" component={About}/>
-                    <Route exact path="/party/:id"  component={Party}/>
-                    <Route exact path="/pass" component={PlaylistPass}/>
+                    <Route exact path="/" component={Main} />
+                    <Route exact path="/create" component={Main} />
+                    <Route exact path="/join" component={Main} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/party/:id" component={Party} />
+                    <Route exact path="/pass" component={PlaylistPass} />
                 </Switch>
             </div>
-            <RightBar/>
-            <Footer/>
+            <RightBar />
+            <Footer />
         </Router>
     );
 }
