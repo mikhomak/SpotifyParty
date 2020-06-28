86fda383e8b10637cf628c9380b760d4d4ef3d44
Misha <homakhaker@gmail.com>
2020-06-28 22:57:15 +0200

    reworked login

---
+++ back/src/index.js
@@ -0,0 +1,37 @@
+
+const express = require("express");
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const port = 3500; // default port to listen
+// define a route handler for the default home page
+
+
+
+app.get("/", (req, res) => {
+
+    
+console.log(process.env.SPOTIFY_CLIENT_ID);
+    res.send("Hello world!");
+});
+
+app.get('/login', function (req, res) {
+    var scopes = 'user-read-private user-read-email';
+    res.redirect('https://accounts.spotify.com/authorize' +
+        '?response_type=code' +
+        '&client_id=' + process.env.SPOTIFY_CLIENT_ID +
+        (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+        '&redirect_uri=' + encodeURIComponent('http://localhost:3500/yes'));
+});
+
+
+app.get('/yes', function (req, res) {
+    console.log('yes');
+})
+
+// start the Express server
+app.listen(port, () => {
+
+
+    console.log(`server started at http://localhost:${port}`);
+});
