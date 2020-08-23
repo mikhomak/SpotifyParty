1cc41d1d5f6c44a0d5091864f08231a509636ba2
Misha <homakhaker@gmail.com>
2020-08-23 18:01:35 +0200

    fixing db

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,10 +1,17 @@
 import { Party } from './entites/Party';
 import {__prod__} from './constants';
 import { MikroORM } from '@mikro-orm/core';
+import path from 'path';
 
 export default {
+    migrations: {
+        path: path.join(__dirname, "./migrations"),
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
     entities: [Party],
     dbName: 'spotyfParty',
     type: 'postgresql',
+    user: 'postgres',
+    password: 'postgres',
     debug: !__prod__
 } as Parameters<typeof MikroORM.init>[0];
