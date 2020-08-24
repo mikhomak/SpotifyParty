a81d1fa67c9036120ba61cffdb5f8b70584bd59c
Misha <homakhaker@gmail.com>
2020-08-24 17:54:03 +0200

    Added song entity

--- server/src/entities/PartyModel.ts
+++ server/src/entities/PartyModel.ts
@@ -19,4 +19,5 @@ export class PartyModel {
     @Field(() => String)
     @Property()
     name: String;
+
 }
---
+++ server/src/entities/SongModel.ts
@@ -0,0 +1,37 @@
+import { PrimaryKey, Property, Entity } from "@mikro-orm/core";
+import { ObjectType, Field, Int } from "type-graphql";
+
+@ObjectType()
+@Entity()
+export class SongModel {
+    @Field(() => Int)
+    @PrimaryKey()
+    id!: number;
+
+    
+    @Field(() => String)
+    @Property()
+    name!: String;
+
+    @Field(() => String)
+    @Property()
+    artist!: String;
+
+    @Field(() => String)
+    @Property()
+    url!: String;
+
+    @Field(() => String)
+    @Property()
+    imageUrl: String;
+
+    @Field(() => Int)
+    @Property()
+    downVotes: number = 0;
+
+    @Field(() => Int)
+    @Property()
+    upVotes: number = 0;
+
+
+}
