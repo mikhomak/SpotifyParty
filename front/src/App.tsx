860dcb32d94b593b28e8b7320d2dd43641135350
Misha <homakhaker@gmail.com>
2020-08-26 23:01:39 +0200

    WIP adding chakra ui

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -9,28 +9,34 @@ import { Party } from "./models/party/Party";
 import { PlaylistPass } from './models/playlistPass/PlaylistPass';
 import { RightBar } from './components/rightBar/RightBar';
 import { Provider, createClient } from 'urql'
+import { ThemeProvider, CSSReset } from '@chakra-ui/core'
+import customTheme from "./theme"
 
-const client = createClient({ url: 'http://localhost:3700/graphql'});
+
+const client = createClient({ url: 'http://localhost:3700/graphql' });
 
 function App() {
     return (
-        <Provider value={client}>
-            <Router>
-                <Navbar />
-                <div className="App"> 
-                    <Switch>
-                        <Route exact path="/" component={Main} />
-                        <Route exact path="/create" component={Main} />
-                        <Route exact path="/join" component={Main} />
-                        <Route exact path="/about" component={About} />
-                        <Route exact path="/party/:id" component={Party} />
-                        <Route exact path="/pass" component={PlaylistPass} />
-                    </Switch>
-                </div>
-                <RightBar />
-                <Footer />
-            </Router>
-        </Provider>
+        <ThemeProvider theme={customTheme}>
+            <CSSReset />
+            <Provider value={client}>
+                <Router>
+                    <Navbar />
+                    <div className="App">
+                        <Switch>
+                            <Route exact path="/" component={Main} />
+                            <Route exact path="/create" component={Main} />
+                            <Route exact path="/join" component={Main} />
+                            <Route exact path="/about" component={About} />
+                            <Route exact path="/party/:id" component={Party} />
+                            <Route exact path="/pass" component={PlaylistPass} />
+                        </Switch>
+                    </div>
+                    <RightBar />
+                    <Footer />
+                </Router>
+            </Provider>
+        </ThemeProvider>
     );
 }
 
--- front/src/models/createParty/CreateParty.tsx
+++ front/src/models/createParty/CreateParty.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Button } from "../../components/UI/button/Button";
 import { Input } from "../../components/UI/input/Input";
 import './CreateParty.css';
-import { Formik } from 'formik';
+import { Formik, Form } from 'formik';
 import { useCreatePartyMutation } from "../../generated/graphql";
 import { toErrorMap } from "../../utils/toErrorMap";
+import { InputField } from "../../components/UI/inputField/InputFields";
+import { Box, Radio, RadioGroup } from "@chakra-ui/core";
 
 interface CreatePartyProps {
 
@@ -18,33 +20,48 @@ interface CreatePartyState {
 export const CreateParty = () => {
 
 
-    const [,createParty] = useCreatePartyMutation();
+    const [, createParty] = useCreatePartyMutation();
+
+    function f(): void { };
+    function saveName(): void { };
 
     return (
         <div className='CreateParty'>
 
 
             <Formik
-            initialValues={{name: "", isPrivate: false}}
-            onSubmit={async (values,{setErrors})=>{
-                const response = await createParty(values);
-                if(response.data?.createParty.errors){
-                    setErrors(toErrorMap(response.data.createParty.errors));
-                }
-            }}>
-
+                initialValues={{ name: "", isPrivate: false }}
+                onSubmit={async (values, { setErrors }) => {
+                    const response = await createParty(values);
+                    if (response.data?.createParty.errors) {
+                        setErrors(toErrorMap(response.data.createParty.errors));
+                    }
+                }}>
+                {({ isSubmitting }) => (
+                    <Form>
+                        <InputField name="name" placeholder="enter name..." label="Name of the Party" />
+                        <RadioGroup defaultValue="2" spacing={5} isInline>
+                            <Radio value="1">
+                                Radio
+                            </Radio>
+                            <Radio value="2">
+                                Radio
+                            </Radio>
+                        </RadioGroup>
+                    </Form>
+                )}
             </Formik>
 
 
 
-            <span>Name:</span>
-            <Input placeholder='Name of the partty' onChangeEvent={this.saveName} />
+            {/* <span>Name:</span>
+            <Input placeholder='Name of the partty' onChangeEvent={saveName} />
             <span>Public?</span><br />
             <input type='radio' /> Private <br />
             <input type='radio' /> Public<br />
             <div className="Buttons">
-                <Button actionEvent={this.f} name='Create' />
-            </div>
+                <Button actionEvent={f} name='Create' />
+            </div> */}
         </div>
     );
 
