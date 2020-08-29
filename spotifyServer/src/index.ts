ade698ba7aae7a190eaf312ada0a6d7c9026b0b3
Misha <homakhaker@gmail.com>
2020-08-29 18:35:45 +0200

    added login to spotify

--- spotifyServer/src/index.ts
+++ spotifyServer/src/index.ts
@@ -1,7 +1,7 @@
 
 import express from 'express';
 import cors from 'cors';
-
+import { PORT, SPOTIFY_TOKEN } from './constants';
 
 
 const main = async () => {
@@ -18,15 +18,21 @@ const main = async () => {
 
     app.get('/spotifyLogin',
         async function (req, res, next) {
-            res.redirect('https://google.com');
+            const scopes = 'user-read-private user-read-email';
+            res.redirect('https://accounts.spotify.com/authorize' +
+                '?response_type=code' +
+                '&client_id=' + SPOTIFY_TOKEN +
+                (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+                '&redirect_uri=' + encodeURIComponent('http://localhost:3800/spotifyCallback'));
         });
 
     app.get('/spotifyCallback',
         async function (req, res, next) {
-            res.redirect('https://amazon.com');
+            res.send('hey');
+            //res.redirect('https://amazon.com');
         });
-    app.listen(3800, () => {
-        console.log("server started on port 3700");
+    app.listen(PORT, () => {
+        console.log("server started on port " + PORT);
     });
 }
 
