2bcd0a71d72e067e9a155f2b5d3cf1a7df7ef4f5
Misha <homakhaker@gmail.com>
2020-08-30 21:49:21 +0200

    added arrow icons

--- front/src/components/queueSongs/songModel/SongModel.tsx
+++ front/src/components/queueSongs/songModel/SongModel.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './SongModel.css';
-import { Box, Image, FormLabel } from "@chakra-ui/core";
+import { Box, Image, FormLabel, Icon } from "@chakra-ui/core";
 
 interface SongModelProps {
     name: string;
@@ -13,8 +13,8 @@ export const SongModel: React.FC<SongModelProps> = (props) => {
     return (
         <Box d='flex' flexDirection='column' maxW='300px' >
             <Box ml={1} w='100%' display="flex" alignItems='center' justifyContent='space-between' >
-                <FormLabel>1</FormLabel>
-                <FormLabel right='auto'>2</FormLabel>
+                <FormLabel color='tomato'>1<Icon name='arrow-down'/></FormLabel>
+                <FormLabel color='Green'>2<Icon name='arrow-up'/></FormLabel>
             </Box>
             <Image size='300' src='https://i.scdn.co/image/ab67616d00001e020b73f9c0b28314ae14ad9e20' />
             <Box display='flex' alignItems='center' justifyContent='center' flexDirection='column'>
